3ca7aebd16947bf8c15879fec88c5b62f506f1c7
watercereal <watercereal11@gmail.com>
2026-03-19 12:20:35 -0400

    add module template

---
+++ src/main/java/a/b/c/templ/Module.java
@@ -0,0 +1,15 @@
+package a.b.c.templ;
+
+import a.b.c.ModuleConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+
+public class Module
+{
+    public String help() { return "no help"; }
+    
+    public void init(ModuleConfig.ModuleCfg cfg) {}
+    
+    public void onTick(ClientTickEvent event, ModuleConfig.ModuleCfg cfg) {}
+}
